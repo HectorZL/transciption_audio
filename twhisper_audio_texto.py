9ef1310578c22c755599a9b0f1a966c4b9d4a395
Hector Zambrano <zambranohector2002@gmail.com>
2023-04-05 10:30:41 -0500

    add a new readme with license and rebase to branch remote to local

--- twhisper_audio_texto.py
+++ twhisper_audio_texto.py
@@ -8,13 +8,13 @@ warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using F
 model = whisper.load_model("medium")
 
 # Ejecuta la transcripción y almacena el resultado en un diccionario
-result = model.transcribe("/home/mrblue/Documentos/PruebaW/pw.m4a")
+result = model.transcribe("/home/insertpathto_archive")
 
-output_dir = "/home/mrblue/Documentos/PruebaW"
+output_dir = "/home/insertpathto_folderoutput"
 
 # Accedemos al valor de la clave 'text' para obtener la transcripción en formato de texto 
 texto_resultado = result["text"]
 
 # Creamos el archivo de texto y escribimos el resultado de la transcripción
-with open(os.path.join(output_dir, "prueba.txt"), "w") as archivo_txt:
+with open(os.path.join(output_dir, "nameof_archive.txt"), "w") as archivo_txt:
     archivo_txt.write(texto_resultado)

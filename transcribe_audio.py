10cf38baa6aca7317a061b6e29cd36d7c4534421
Hector Zambrano <zambranohector2002@gmail.com>
2023-04-07 16:51:15 -0500

    <feat>(<transcribe.py>): <add a bar and see the progess>
    <add a new feacture in the branch dev>
    
    <add the eta>
    
    By HectorZL

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,18 +1,20 @@
 import os
+import time
 import tkinter as tk
+import pydub
 from tkinter import filedialog, messagebox
+from pydub import AudioSegment
 import whisper
-import warnings
 
-warnings.filterwarnings("ignore")
 
 class WhisperGUI:
-
     def __init__(self, master):
         self.master = master
         self.master.title("Transcripción de audio con Whisper")
 
         self.audio_file = None
+        self.progress_canvas = None
+        self.max_time = None  # Duración máxima del archivo de audio
 
         # widgets
         self.select_audio_button = tk.Button(self.master, text="Seleccionar archivo de audio", command=self.select_audio)
@@ -20,16 +22,23 @@ class WhisperGUI:
         self.output_label = tk.Label(self.master, text="Seleccione un archivo de audio y modelo para comenzar la transcripción")
 
         self.model_var = tk.StringVar(self.master)
-        self.model_var.set("base")  # Default value
+        self.model_var.set("base")  # Valor predeterminado
         self.model_menu = tk.OptionMenu(self.master, self.model_var, "base", "small", "medium", "large")
 
         self.output_dir = os.getcwd()
 
-        # disposición de los widgets en la ventana
+        # Disposición de los widgets en la ventana
         self.select_audio_button.pack()
         self.model_menu.pack()
         self.transcribe_button.pack()
         self.output_label.pack()
+
+        # Crear el Canvas de la barra de progreso
+        self.progress_canvas = tk.Canvas(self.master, width=200, height=10, borderwidth=1, relief="solid")
+        self.progress_canvas.pack()
+
+        # Crear un rectángulo para mostrar el progreso actual
+        self.progress_rect = self.progress_canvas.create_rectangle(0, 0, 0, 10, fill="blue", width=0)
 
     def select_audio(self):
         self.audio_file = filedialog.askopenfilename(
@@ -37,6 +46,10 @@ class WhisperGUI:
             filetypes=[("Archivo de audio", "*.wav;*.mp3;*.ogg;*.flac;*.m4a")]
         )
         if self.audio_file:
+            # Obtener la duración del archivo de audio
+            audio = AudioSegment.from_file(self.audio_file)
+            self.max_time = audio.duration_seconds
+
             messagebox.showinfo(
                 "Archivo seleccionado", f"Ha seleccionado el archivo de audio: {os.path.basename(self.audio_file)}")
 
@@ -48,6 +61,14 @@ class WhisperGUI:
             # Cargamos el modelo seleccionado de Whisper
             model_name = self.model_var.get()
             model = whisper.load_model(model_name)
+
+            # Actualizamos la barra de progreso cada segundo
+            time_elapsed = 0
+            while time_elapsed <= self.max_time:
+                time.sleep(1)
+                time_elapsed += 1
+                progress = int(100 * time_elapsed / self.max_time)
+                self.update_progressbar(progress)
 
             result = model.transcribe(self.audio_file)
 
@@ -61,8 +82,19 @@ class WhisperGUI:
         else:
             messagebox.showerror("Error", "No ha seleccionado ningún archivo de audio")
 
+    def update_progressbar(self, progress):
+        # Actualización de la barra de progreso
+        canvas_width = 200
+        progress_fraction = progress / 100
+        progress_width = int(canvas_width * progress_fraction)
+        self.progress_canvas.coords(self.progress_rect, 0, 0, progress_width, 10)
+
+        # Actualización de la etiqueta de progreso
+        self.output_label["text"] = f"Transcribiendo... {progress}% completado"
+        self.master.update_idletasks()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    gui = WhisperGUI(root)
+    whisper_gui = WhisperGUI(root)
     root.mainloop()
